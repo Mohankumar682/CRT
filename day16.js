9f5027eaf08330b25f4909224f5e8ef2310c01d4
Mohankumar682 <mk7341924@gmail.com>
2025-08-07 08:59:17 +0530

    Add files via upload

---
+++ day16.js
@@ -0,0 +1,13 @@
+let a="kumar"
+let b=89
+let c="mohan"
+let d=1
+console.log(c+a)
+for(let i=0; i<=b;i++){
+    console.log(i+d)
+}
+if(b>d)
+    console.log("yes")
+else{
+    console.log("no")
+}
