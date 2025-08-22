c6f8439df5f88af50de2f911b73c70bb6e1bf0a2
Mohankumar682 <mk7341924@gmail.com>
2025-08-22 09:54:44 +0530

    Add files via upload

---
+++ day20.js
@@ -0,0 +1,4 @@
+document.querySelector("p").innerHTML="this is"
+document.getElementById("a1").innerHTML="yes"
+document.querySelector("p:last-of-type").innerHTML="This an ms"
+document.querySelector("p:first-of-type").innerHTML="This an ms"
